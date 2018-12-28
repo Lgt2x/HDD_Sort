d50e6993344dca607a9f86fc840cf15705e31ee5
Lgt2x <44813937+Lgt2x@users.noreply.github.com>
2018-12-28 23:50:58 +0100

    Outputs in a text file

--- ListFiles.py
+++ ListFiles.py
@@ -1,17 +1,21 @@
 import os
 import glob
 
-pathInit = input()
+pathInit = input("Path to HDD: ")
+dest = input("Dossier destination de la liste: ")
+listFile = open(dest+"/"+"list_"+os.path.basename(pathInit)+".txt","w+")
 
-def Unfold(folder):
+def Unfold(folder, listFile):
     children = glob.glob(folder+"/*")
     for child in children:
         if os.path.isdir(child):
-            Unfold(child)
+            Unfold(child, listFile)
         else:
             file = os.path.splitext(os.path.basename(child))
             if file[1] in [".avi",".mkv",".mp4"]:
-                print(file[0])
+                listFile.write(file[0]+"\n")
     
 
-Unfold(pathInit)
+Unfold(pathInit, listFile)
+
+listFile.close()
