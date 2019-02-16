b216c0979ede6ed2ecc0709bb2ca3be71d9f6e71
Lgt2x <louisgom2001@gmail.com>
2019-02-16 21:54:06 +0100

    Now merges and sorts .csv files generated by ListFiles

--- Merge.py
+++ Merge.py
@@ -1,13 +1,17 @@
-import os
 import glob
+import sys
 
-folderInit = input("Path to folder containing lists: ")
-mergeFile = open(folderInit+"/merge_HDDs.txt","w+")
+if __name__ == "__main__":
+    folder = sys.argv[1]
 
-children = glob.glob("/Users/Louis/Desktop/HDD_Sort/*list_*.txt")
+    children = glob.glob(folder+"/*list_*.csv")
+    allMovies = []
+    for movieList in children:
+        with open(movieList, "r") as textList:
+            allMovies += textList.readlines()
 
-for movieList in children:
-    with open(movieList,"r") as textList:
-        mergeFile.write(textList.read())
+    allMovies.sort()
 
-mergeFile.close()
+    with open(folder + "/merge.csv", "w+") as mergeFile:
+        for movie in allMovies:
+            mergeFile.write(movie)
