3aeebd7e3d008fada64e6eeae5869ee676da7f2f
Lgt2x <louisgom2001@gmail.com>
2019-02-16 19:08:09 +0100

    Replace inputs by argv Now outputs .csv

--- ListFiles.py
+++ ListFiles.py
@@ -3,22 +3,29 @@ import glob
 import sys
 
 
-def unfold(folder, name, textfile):
+def unfold(folder, name, textfile, count):
     children = glob.glob(folder + "/*")
     for child in children:
         if os.path.isdir(child):
-            unfold(child, name, textfile)
+            count = unfold(child, name, textfile, count)
         else:
             file = os.path.splitext(os.path.basename(child))
             if file[1] in [".avi", ".mkv", ".mp4"]:
+                count += 1
                 textfile.write(file[0] + ", " + name + "\n")
+    return count
 
 
 if __name__ == "__main__":
+    print()
     try:
-        scriptPath, hddPath, hddName = sys.argv
+        scriptPath, hddPath, destPath, hddName = sys.argv
     except ValueError:
-        print("Wrong number of arguments given (requires 2)")
+        print("\u001b[31mWrong number of arguments given\u001b[0m")
+        print("Usage: python3 [Path to script] [Path to hard drive] [Destination of the list] [Name given to the list]")
     else:
-        with open(hddPath + "/" + "list_" + hddName + ".csv", "w+") as movieList:
-            unfold(hddPath, hddName, movieList)
+        print("\u001b[1mComputing...\u001b[0m")
+        with open(destPath + "/" + "list_" + hddName + ".csv", "w+") as movieList:
+            counter = unfold(hddPath, hddName, movieList, 0)
+        print("Done")
+        print(counter, " entries added to ", str(hddName)+ ".csv")
